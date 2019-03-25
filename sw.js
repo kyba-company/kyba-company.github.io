15c3fa0941d0a939aea8c639fb03f8344ec084d4
xeyka <xeyka75@gmail.com>
2019-03-25 10:43:06 +0200

    1

--- sw.js
+++ sw.js
@@ -1,48 +1,32 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
 importScripts("workbox-v3.6.3/workbox-sw.js");
+
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 workbox.skipWaiting();
 workbox.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
 self.__precacheManifest = [
   {
     "url": "assets/css/main.css",
-    "revision": "4"
+    "revision": "5"
   },
   {
     "url": "assets/img/gallery/5.jpg",
-    "revision": "3"
+    "revision": "5"
   },
   {
     "url": "assets/img/gallery/6.jpg",
-    "revision": "3"
+    "revision": "5"
   },
   {
     "url": "assets/js/bundle.js",
-    "revision": "3"
+    "revision": "5"
   },
   {
     "url": "index.html",
-    "revision": "4"
+    "revision": "5"
   }
 ].concat(self.__precacheManifest || []);
+
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
